1605e4052d320910bb9f74f5eeb6be86d6c77a66
Brandon <myboyknuckles@gmail.com>
2024-02-29 08:08:22 -0600

    Added route, Get all Events endpoint

---
+++ backend/routes/api/event.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const {  requireAuth, authenticationCheck } = require('../../utils/auth');
+const { Op } = require('sequelize');
+
+const { Event, Group, Venue } = require('../../db/models');
+
+const router = express.Router();
+
+router.get('/', async(req, res, next)=>{
+  const allEvents = await Event.findAll({
+    include: [
+      {model: Group, attributes: ['id', 'name', 'city', 'state']},
+      {model: Venue, attributes: ['id', 'city', 'state']}],
+    attributes: {
+      exclude: ['description']
+    }
+  })
+
+  res.json({Events: allEvents})
+})
+
+
+
+
+
+
+module.exports = router;
--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -3,6 +3,7 @@ const sessionRouter = require('./session.js');
 const usersRouter = require('./users.js')
 const groupRouter = require('./group.js')
 const venueRouter = require('./venue.js')
+const eventRouter = require('./event.js')
 
 const { restoreUser } = require('../../utils/auth.js');
 
@@ -11,6 +12,7 @@ router.use('/session', sessionRouter)
 router.use('/users', usersRouter)
 router.use('/groups', groupRouter)
 router.use('/venues', venueRouter)
+router.use('/events', eventRouter)
 
 router.post('/test', (req, res) => {
   res.json({ requestBody: req.body });
