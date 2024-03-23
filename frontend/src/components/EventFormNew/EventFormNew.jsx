b5b1b395ae3bc9fccaf27fe293f2b255588f673b
Brandon <myboyknuckles@gmail.com>
2024-03-23 17:05:27 -0500

    States added to EventFormNew

--- frontend/src/components/EventFormNew/EventFormNew.jsx
+++ frontend/src/components/EventFormNew/EventFormNew.jsx
@@ -1,4 +1,17 @@
+import { useDispatch, useSelector } from 'react-redux'
+import { useState, useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+import './EventFormNew.css'
+
 export default function EventFormNew() {
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+  const [errors, setErrors] = useState({})
+
+  const sessionUser = useSelector(state => state.session.user);
+
+  // Need name, type, price, startDate, endDate, Image, description,
+  // need
   return (
     <h1>Hi from EventFormNew</h1>
   )
